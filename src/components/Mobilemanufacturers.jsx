7f7d8c0027891e3bf2244e9241f5a8baae5e146e
jksingh1504 <boby150499@gmail.com>
2022-05-24 11:02:36 +0530

    hello

---
+++ src/components/Mobilemanufacturers.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Mobilemanufacturers = () => {
+  return (
+    <div>
+        <h2>Mobile Manufacturers</h2>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Mobilemanufacturers
---
+++ src/components/Mobileos.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const mobileos = () => {
+  return (
+    <div>
+        <h2>Mobile Operating System</h2>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Windows Phone</li>
+        </ul>
+    </div>
+  )
+}
+
+export default mobileos
